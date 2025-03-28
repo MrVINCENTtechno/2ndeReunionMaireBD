33866c585e782b60e59690249245dde4dbc977ee
MrVINCENTtechno <college@benfx.fr>
2025-03-28 17:02:50 +0100

    liste des taches détail et mise en forme

--- script.js
+++ script.js
@@ -17,24 +17,24 @@ const answers = [
   "1. Réunion | 2. Ébauche | 3. Plan | 4. Vérification | 5. Présentation | 6. Réunion | 7. Choix terrain | 8. 3D | 9. Réunion finale",
   "Je regrette mais tu es capable de répondre à cette question par toi même",
   "Aucun travaux de construction. Seulement imaginer le projet et réaliser des représentations  (plans, vue 3D, maquettes etc..)",
-  "les dimensions d'un conteneurs\n  ce que le logement et la résidence doivent avoir et comment tout doit fonctionner\n  les outils informatiques et le materiel nécessaires",
+  "les dimensions d'un conteneurs<br>  ce que le logement et la résidence doivent avoir et comment tout doit fonctionner<br>  les outils informatiques et le materiel nécessaires",
   "OUI !!! Le maire nous donnera les détails supplémentaires pendant les réunions.",
   "9 tâches",
   "Je regrette mais tu es capable de répondre à cette question par toi même",
-  "Durée des tâches en nombre d'heure de cours (9h)\n  1.demi | 2.une | 3.deux| 4.demi | 5.demi | 6.demi | 7.une | 8.deux | 9.une",
+  "Durée des tâches en nombre d'heure de cours (9h)<br>  1.demi | 2.une | 3.deux| 4.demi | 5.demi | 6.demi | 7.une | 8.deux | 9.une",
   "2.Ebauche | 3.Plan d'aménagement | 8.Implantation géographique 3D"
 ];
 
 const tasks = [
-  "1. Réunion 'exigences et description fonctionnelle'",
-  "2. Ébauche main levée",
-  "3. Plan informatique",
-  "4. Vérification conformité",
-  "5. Présentation au Maire", 
-  "6. Réunion 'Résidence Étudiante : solutions'",
-  "7. Choix du terrain",
-  "8. Modélisation 3D",
-  "9. Présentation finale"
+  "1. Réunion 'exigences et description fonctionnelle'<br>",
+  "2. Ebauche<BR>dessiner à la main une ébauche et la montrer au maire",
+  "3. Plan d'aménagement<BR>réaliser l'ammenagement avec un outil de description informatique",
+  "4. Vérification<BR>Vérifier qu'il n'y ait aucun écart entre le travail réalisé et les exigences et la description fonctionnelle (tâche N°1)",
+  "5. Présentation<BR>Présenter au maire son plan d'aménagement", 
+  "6. Réunion 'Résidence Étudiante : solutions'<BR>Définir les bases de notre résidence étudiante : nombre, orientation, implantation et rendu de notre présentation finale",
+  "7. Choix du terrain<BR>Utiliser des cartes géographiques, des maquettes et Internet pour choisir le terrain de la résidence",
+  "8. Implantation géographique 3D<BR>Implanter la résidence sur le terrain avec un outil de modélisation 3D",
+  "9. Réunion 'Présentation finale'<BR>"
 ];
 
 // Initialisation
@@ -80,7 +80,7 @@ function showTasks() {
   dialogueBox.innerHTML = `
     <p><strong>Liste des tâches :</strong></p>
     <ul>
-      ${tasks.map(task => `<li>${task}</li>`).join('')}
+      ${tasks.map(task => `${task}`).join('')}
     </ul>
   `;
 }
