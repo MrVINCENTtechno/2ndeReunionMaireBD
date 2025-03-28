d825b4107905a54c2faf9a76498ffd0062dc19c1
MrVINCENTtechno <college@benfx.fr>
2025-03-28 15:38:38 +0100

    script

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 // Données
 const questions = [
-  "1. Début et fin du projet ?",
-  "2. Étapes dans l'ordre ?", 
-  "3. Livrables pour le Maire ?",
-  "4. Limites du projet ?",
-  "5. Prérequis ?",
-  "6. Détails supplémentaires ?",
-  "7. Nombre de tâches ?",
-  "8. Organisation des tâches ?",
-  "9. Durée des tâches ?",
-  "10. Tâches à valider ?"
+  "1. Par quoi commence le projet et par quoi se termine-til ?",
+  "2. Que faut-il faire et dans quel ordre ?", 
+  "3. Que devons-nous livrer à Mr le Maire ?",
+  "4. À quoi se limite précisément notre projet ?",
+  "5. Que faut-il connaître ?",
+  "6. Le Maire doit-il nous donner d'autres détails ?",
+  "7. Combien de tâches ?",
+  "8. Comment organiser les tâches ?",
+  "9. Combien de temps chaque tâche va-t-elle prendre ?",
+  "10. Quelles tâches faudra-t-il présenter au Maire et obtenir son accord pour continuer ?"
 ];
 
 const answers = [
   "Commencer par l'aménagement d'un conteneur. Et terminer par la livraison de plusieurs représentations (plan, vue 3D, maquette, etc..).",
   "1. Réunion | 2. Ébauche | 3. Plan | 4. Vérification | 5. Présentation | 6. Réunion | 7. Choix terrain | 8. 3D | 9. Réunion finale",
-  "Plans, vues 3D, maquettes...",
-  "Pas de construction, seulement conception",
-  "Dimensions conteneur et besoins logement",
-  "OUI ! Détails lors des réunions",
+  "Fournir plusieurs représentations du projet de construction (plan, vue 3D, maquette, etc..)",
+  "Aucun travaux de construction. Seulement imaginer le projet et réaliser des représentations  (plans, vue 3D, maquettes etc..)",
+  "les dimensions d'un conteneurs\n  ce que le logement et la résidence doivent avoir et comment tout doit fonctionner\n  les outils informatiques et le materiel nécessaires",
+  "OUI !!! Le maire nous donnera les détails supplémentaires pendant les réunions.",
   "9 tâches",
-  "Diagramme de GANTT",
-  "Total 9h : demi-heure à 2h par tâche",
-  "Ébauche, Plan, Implantation 3D"
+  "En repertoriant toutes les tâches à accomplir dans un diagramme de GANTT.",
+  "Durée des tâches en nombre d'heure de cours (9h)\n  1.demi | 2.une | 3.deux| 4.demi | 5.demi | 6.demi | 7.une | 8.deux | 9.une",
+  "2.Ebauche | 3.Plan d'aménagement | 8.Implantation géographique 3D"
 ];
 
 const tasks = [
-  "1. Réunion 'exigences'",
+  "1. Réunion 'exigences et description fonctionnelle'\n",
   "2. Ébauche main levée",
   "3. Plan informatique",
   "4. Vérification conformité",
